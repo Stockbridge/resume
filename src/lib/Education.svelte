3623fa5c05eb08b665e4ce1cebbe575a1f38b374
Stockbridge <chris.d.stockbridge@gmail.com>
2025-11-14 15:30:32 -0800

    migrating data out into json files

--- src/lib/Education.svelte
+++ src/lib/Education.svelte
@@ -1,22 +1,25 @@
 <script lang="ts">
 	import SectionHeader from '$lib/SectionHeader.svelte';
 	import ItemHeader from '$lib/ItemHeader.svelte';
-	export let education: Array<{
+	export let education: {
 		degree: string;
 		school: string;
 		dates: string;
 		description?: string;
-	}>;
+	};
 </script>
 
 <section class="mb-8">
-	<SectionHeader text="Education" />
-	{#each education as edu}
-		<div class="mb-4">
-			<ItemHeader title={edu.degree} dates={edu.dates} subtitle={edu.school} location={undefined} />
-			{#if edu.description}
-				<div class="mt-1 text-sm text-gray-600">{edu.description}</div>
-			{/if}
-		</div>
-	{/each}
+	<SectionHeader text="educationcation" />
+	<div class="mb-4">
+		<ItemHeader
+			title={education.degree}
+			dates={education.dates}
+			subtitle={education.school}
+			location={undefined}
+		/>
+		{#if education.description}
+			<div class="mt-1 text-sm text-gray-600">{education.description}</div>
+		{/if}
+	</div>
 </section>
